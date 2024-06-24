a2bc777728a479759079997de34f2dccb13998e4
Lorenzo Tellez <ltellez@gallopintonetworks.net>
2024-06-24 14:56:03 -0600

    Funciones

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto';
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número del 1 al 10';
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
 
 function intentoUsuario(){
     
 }
+
+asignarTextoElemento('p','Indica un número del 1 al 10');
+asignarTextoElemento('h1', 'Juego del número secreto');

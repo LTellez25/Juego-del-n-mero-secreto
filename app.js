3ee69c4217e6cad0277e49f7042436915aef58b1
Lorenzo Tellez <ltellez@gallopintonetworks.net>
2024-06-30 10:06:32 -0600

    Limpiar Campo

--- app.js
+++ app.js
@@ -10,15 +10,22 @@ function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     if (numeroDeUsuario === numeroSecreto){
         asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else {
+        //El usuario no acertó.
         if (numeroDeUsuario > numeroSecreto){
         asignarTextoElemento('p','El número secreto es menor');
         }else{
             asignarTextoElemento('p','El número secreto es mayor');
         }
         intentos++;
+        limpiarCaja();
     }
     return;
+}
+
+function limpiarCaja(){
+   document.querySelector('#valorUsuario').value ='';
 }
 
 function generarNumeroSecreto(){
